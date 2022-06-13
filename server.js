aaff066d108e9dc049a65d4545b19b913e709cce
aquamarissa <vremapriklucenij16@gmail.com>
2022-06-13 12:48:35 +0300

    cooooors

--- server.js
+++ server.js
@@ -8,10 +8,21 @@ const { disconnect } = require("process");
 // const PORT = process.env.PORT || 5002;
 const app = express();
 const httpServer = http.createServer(app);
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
-app.use(cors({
-  origin: '*'
-}));
+  // intercept OPTIONS method
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  }
+  else {
+    next();
+  }
+}
+app.use(app.router);
+app.use(allowCrossDomain)
 // app.use(express.static(path.join(__dirname, 'build')))
 // app.get('/*', (req, res) => {
 //   res.sendFile(path.join(__dirname, 'build', 'index.html'))
